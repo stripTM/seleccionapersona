ce8b7e400f4541d4f73b58dce4209f9ae333e648
Fernando García <fernando.garciagomez.01@telefonica.com>
2021-02-25 02:28:43 +0100

    Implementar undo y posiblididad de mostrar/ocultar formulario

--- api.php
+++ api.php
@@ -1,66 +1,105 @@
 <?php
 $ORIGEN = __DIR__.'/photoCollection.json';
-$SESSION_COLLECTION = sys_get_temp_dir().'/photoCollectionSession.json';
+$SESSION_PENDING_LIST = sys_get_temp_dir().'/photoPendingSession.json';
+$SESSION_HISTORY_LIST = sys_get_temp_dir().'/photoHistorySession.json';
 $SESSION_SELECTION = sys_get_temp_dir().'/photoSelectionSession.json';
 
-function addId ($item, $n)
-        {
-            console.log("FE");
-            $item->id = "+ $n";
-            return $item;
-        };
-
-//header('Content-Type: application/json');
-$action = isset($_POST['a']) ? $_POST['a'] : 'list';
+$action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'list';
 
 switch ($action) {
     case 'reset':
         // Cargamos las fotos las añadimos un id y fijamos la derecha e izquierda
-        $photoCollection = json_decode(file_get_contents($ORIGEN));
+        $photoCollectionData = json_decode(file_get_contents($ORIGEN));
+        // Ordenar como una pila
+        $photoCollection = $photoCollectionData->photos;
         $n = 0;
-        $photoCollectionId = array_map(
-            function($item, $n) {
-                $item->id = $n;
-                $n++;
-                return $item;
-            },
-            $photoCollection,
-            array_keys($photoCollection)
+        $photoCollectionId = array_reverse(
+            array_map(
+                function($item, $n) {
+                    $item->id = "$n";
+                    $n++;
+                    return $item;
+                },
+                $photoCollection,
+                array_keys($photoCollection)
+            )
         );
         $selectionPhotos = new stdclass();
         $selectionPhotos->photoLeft = array_pop($photoCollectionId);
         $selectionPhotos->photoRight = array_pop($photoCollectionId);
+        $selectionPhotos->refreshTime = $photoCollectionData->refreshTime;
 
-        file_put_contents ($SESSION_COLLECTION, json_encode(array_values($photoCollectionId)));
-        file_put_contents ($SESSION_SELECTION, json_encode($selectionPhotos));
+        $history = [];
 
-        header('Location: .');
-        //header('Content-Type: text/plain');
-        //var_dump($selectionPhotos, $photoCollectionId);
-        break;
-    case 'pop':
-        $photoCollection = json_decode(file_get_contents ($SESSION_COLLECTION));
-        $selectionPhotos = json_decode(file_get_contents ($SESSION_SELECTION));
-        $idRemove = isset($_POST['n']) ? intval($_POST['n']) : null;
-        if ($idRemove === $selectionPhotos->photoLeft->id) {
-            $selectionPhotos->photoLeft = array_pop($photoCollection);
-            unset($photoCollection[$idRemove]);
-        }
-        elseif ($idRemove === $selectionPhotos->photoRight->id) {
-            $selectionPhotos->photoRight = array_pop($photoCollection);
-        }
-        file_put_contents ($SESSION_COLLECTION, json_encode(array_values($photoCollection)));
+        file_put_contents ($SESSION_PENDING_LIST, json_encode(array_values($photoCollectionId)));
         file_put_contents ($SESSION_SELECTION, json_encode($selectionPhotos));
+        file_put_contents ($SESSION_HISTORY_LIST, json_encode($history));
+
         header('Content-Type: application/json');
         echo(json_encode($selectionPhotos));
         break;
+    case 'delete':
+        $photoCollection = json_decode(file_get_contents ($SESSION_PENDING_LIST));
+        $history = json_decode(file_get_contents ($SESSION_HISTORY_LIST));
+        $selectionPhotos = json_decode(file_get_contents ($SESSION_SELECTION));
+        $newHistoryItem = new stdclass();
+
+        $idDetele = isset($_POST['n']) ? $_POST['n'] : null;
+        $photoPosition = isset($_POST['position']) ? $_POST['position'] : null;
+        if ($idDetele === $selectionPhotos->photoLeft->id) {
+            $newHistoryItem->photo = $selectionPhotos->photoLeft;
+            $selectionPhotos->photoLeft = array_pop($photoCollection);
+        }
+        elseif ($idDetele === $selectionPhotos->photoRight->id) {
+            $newHistoryItem->photo = $selectionPhotos->photoRight;
+            $selectionPhotos->photoRight = array_pop($photoCollection);
+        }
+
+        if($newHistoryItem->photo) {
+            $newHistoryItem->position = $photoPosition;
+            $history[] = $newHistoryItem;
+        }
+
+        file_put_contents ($SESSION_PENDING_LIST, json_encode(array_values($photoCollection)));
+        file_put_contents ($SESSION_SELECTION, json_encode($selectionPhotos));
+        file_put_contents ($SESSION_HISTORY_LIST, json_encode($history));
+
+        header('Content-Type: application/json');
+        echo(json_encode($selectionPhotos));
+        break;
+    case 'undo':
+        $photoCollection = json_decode(file_get_contents ($SESSION_PENDING_LIST));
+        $history = json_decode(file_get_contents ($SESSION_HISTORY_LIST));
+        $selectionPhotos = json_decode(file_get_contents ($SESSION_SELECTION));
+        $undoPhoto = array_pop($history);
+
+        if($undoPhoto) {
+            if($undoPhoto->position === 'photoLeft') {
+                $photoCollection[] = $selectionPhotos->photoLeft;
+                $selectionPhotos->photoLeft = $undoPhoto->photo;
+            } else {
+                $photoCollection[] = $selectionPhotos->photoRight;
+                $selectionPhotos->photoRight = $undoPhoto->photo;
+            }
+        }
+
+        file_put_contents ($SESSION_PENDING_LIST, json_encode(array_values($photoCollection)));
+        file_put_contents ($SESSION_SELECTION, json_encode($selectionPhotos));
+        file_put_contents ($SESSION_HISTORY_LIST, json_encode($history));
+
+        header('Content-Type: application/json');
+        echo(json_encode($selectionPhotos));
+        break;
+    case 'collection':
+        header('Content-Type: application/json');
+        readfile($SESSION_PENDING_LIST);
+        break;
+    case 'history':
+        header('Content-Type: application/json');
+        readfile($SESSION_HISTORY_LIST);
+        break;
     default:
         header('Content-Type: application/json');
-        if(isset($_GET['a']) && $_GET['a'] === 'collection') {
-            readfile($SESSION_COLLECTION);
-        }
-        else {
-            readfile($SESSION_SELECTION);
-        }
+        readfile($SESSION_SELECTION);
         break;
 }
--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // Enganche con los elementos de la página
 const imgLeft = document.getElementById('photoLeft');
 const imgRight = document.getElementById('photoRight');
-// Controlar el clic en las fotos
-imgLeft.addEventListener('click', (e) => setNewPhoto(e.target));
-imgRight.addEventListener('click', (e) => setNewPhoto(e.target));
-let ready = false;
-let timeoutID = null;
+const admin = document.getElementById('admin');
+const resetButton = admin.querySelector("input[value=reset]");
+const undoButton = admin.querySelector("input[value=undo]");
+const closeButton = admin.querySelector(".close");
 
 // Cargar las fotos del servidor
 const load = function () {
@@ -18,7 +17,9 @@ const load = function () {
         setPhoto(imgLeft, res.photoLeft);
         setPhoto(imgRight, res.photoRight);
         ready = true;
-        timeoutID = setTimeout(load, 5000);
+        if (res.refreshTime) {
+            timeoutID = setTimeout(load, Number(res.refreshTime));
+        }
     });
 }
 
@@ -43,8 +44,9 @@ const setNewPhoto = function (img) {
 
     // request options
     const data = new URLSearchParams();
-    data.append('a', 'pop');
+    data.append('a', 'delete');
     data.append('n', imgChange.dataset.id);
+    data.append('position', imgChange.id);
     const options = {
         method: 'POST',
         body: data
@@ -57,5 +59,55 @@ const setNewPhoto = function (img) {
             setPhoto(imgRight, res.photoRight);
         });
 }
+
+const reset = function (e) {
+    // request options
+    const data = new URLSearchParams();
+    data.append('a', 'reset');
+    const options = {
+        method: 'POST',
+        body: data
+    }
+    // send POST request
+    fetch('api.php', options)
+        .then(res => res.json())
+        .then(res => {
+            setPhoto(imgLeft, res.photoLeft);
+            setPhoto(imgRight, res.photoRight);
+        });
+    e.preventDefault();
+}
+
+const undo = function (e) {
+    // request options
+    const data = new URLSearchParams();
+    data.append('a', 'undo');
+    const options = {
+        method: 'POST',
+        body: data
+    }
+    // send POST request
+    fetch('api.php', options)
+        .then(res => res.json())
+        .then(res => {
+            setPhoto(imgLeft, res.photoLeft);
+            setPhoto(imgRight, res.photoRight);
+        });
+    e.preventDefault();
+}
+
+// Controlar el clic en las fotos
+imgLeft.addEventListener('click', (e) => setNewPhoto(e.target));
+imgRight.addEventListener('click', (e) => setNewPhoto(e.target));
+let ready = false;
+let timeoutID = null;
+
+// Formulario de administración
+admin.addEventListener('click', (e) => e.target.classList.toggle('active'));
+admin.addEventListener('submit', (e) => e.preventDefault());
+closeButton.addEventListener('click', (e) => admin.classList.remove('active'));
+resetButton.addEventListener('click', reset);
+undoButton.addEventListener('click', undo);
+
 
 load();
